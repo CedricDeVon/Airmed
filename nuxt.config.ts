de84f3d1a7fbb3b59bac7115fd5a2289509758fb
CedricDeVon <vaughncedriclipardoaraneta@gmail.com>
2024-09-14 17:02:57 +0800

    Implemented base configurations

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,4 +5,35 @@ export default defineNuxtConfig({
     // prerender index route by default
     '/': { prerender: true },
   },
-});
+  modules: [
+    '@vueuse/nuxt',
+    '@pinia/nuxt',
+    '@nuxtjs/color-mode',
+    'nuxt-icon',
+    'nuxt-headlessui',
+    'nuxt-vitest',
+    '@nuxt/devtools'
+  ],
+
+  experimental: {
+    reactivityTransform: true
+  },
+
+  css: ['~/assets/css/style.scss'],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {}
+    }
+  },
+
+  colorMode: {
+    classSuffix: ''
+  },
+
+  headlessui: {
+    prefix: ''
+  },
+  compatibilityDate: '2024-08-24'
+})
